f3f70780b1110e03f12a4c777adbb02077d92bdb
mvictor10 <mvictordossantos.limite@gmail.com>
2023-08-26 01:52:15 -0300

    feat: deixando código orientando a objeto 'app_debug.py'

--- app_debug.py
+++ app_debug.py
@@ -1,56 +1,39 @@
-import cv2 as cv
+import cv2
 import threading
 
-class App(threading.Thread):
-	def __init__(self):
-		super().__init__()
-		# Carregar o classificador em cascata pré-treinado
-		self.face_cascate = cv.CascadeClassifier(cv.data.haarcascades + 'haarcascade_frontalface_default.xml')
-		self.cap = cv.VideoCapture(0, cv.CAP_DSHOW)
-		self.cap.set(cv.CAP_PROP_FRAME_WIDTH,640)
-		self.cap.set(cv.CAP_PROP_FRAME_HEIGHT,480)
-		self.cap.set(cv.CAP_PROP_FPS,120)
-		self.cap.set(cv.CAP_PROP_FOURCC, cv.VideoWriter_fourcc(*'MJPG'))
-		self.img = None
-		self.runnning = None
-		self.gray = None 
-		self.faces = None
-		self.color = (0,255,0)
-		self.font = cv.FONT_HERSHEY_SIMPLEX
-		self.tamanho = 1
-		self.espessura = 2
-		
-	def run(self):
-		#ler o quadro atual do video
-		while self.runnning:
-			ret, img = self.cap.read()
-			if not ret:
-				print("Não foi possivel ler o quadro atual!")
-				break
+class FaceDetection:
+    def __init__(self):
+        self.face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+        self.font = cv2.FONT_HERSHEY_SIMPLEX
+        self.cor = (0, 255, 0)
+        self.tamanho = 1
+        self.espessura = 2
+        self.cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+        self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+        self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+        self.cap.set(cv2.CAP_PROP_FPS, 30)
+        self.cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'MJPG'))
 
-			# converter para a escala de cinza
-			self.gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    def detect_faces(self):
+        while True:
+            ret, img = self.cap.read()
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            faces = self.face_cascade.detectMultiScale(gray, 1.1, 4)
 
-			# Detectar faces
-			self.faces = self.face_cascate.detectMultiScale(self.gray, 1.1,4)
+            for (x, y, w, h) in faces:
+                cv2.rectangle(img, (x, y), (x + w, y + h), self.cor, self.espessura)
+                cv2.putText(img, 'Rosto Detectado.', (x, y - 10), self.font, self.tamanho, self.cor, self.espessura, cv2.LINE_AA)
 
-			# Desenhar retângulos ao redor das faces e adicionar legenda
-			for (x, y, w, h) in self.faces:
-				cv.rectangle(self.img,(x,y), (x+w, y+h), self.color, self.espessura)
-				cv.putText(self.img, 'Rosto Detectado', (x,y-10),self.font, self.tamanho, self.color, self.espessura,cv.LINE_AA)
-			
-			cv.imshow('Reconhecimento Facial', self.img)
+            cv2.imshow('img', img)
 
-			# Parar se a tecla 'q' for pressionada
-	    	if cv.waitKey(1) & 0xFF == ord('q'):
-	        	break
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
 
-	def stop(self):
-		self.runnning = False
-		self.cap.release()
+    def start(self):
+        thread = threading.Thread(target=self.detect_faces)
+        thread.start()
+        thread.join()
 
 if __name__ == "__main__":
-	app = App()
-	app.start()
-
-
+    face_detection = FaceDetection()
+    face_detection.start()
