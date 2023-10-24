bb998a149c9fac528da7143aaa73c69246d27f3b
Manoel Vitor <manoelvitorsilvasantos@gmail.com>
2023-10-24 13:31:42 -0300

    Update twilio.py

--- utils/twilio.py
+++ utils/twilio.py
@@ -2,8 +2,8 @@ from twilio.rest import Client
 
 class SendWhatsappApi:
 	def __init__(self):
-		self.account_sid = 'AC32215330dcd9178564d0b312cffe4531'
-		self.auth_token = 'ef133ec7862ed1b0a6cfd24313bd9ec8'
+		self.account_sid = '<ACCOUNT_SID>'
+		self.auth_token = '<AUTH_TOKEN>'
 		self.client = Client(self.account_sid, self.auth_token)
 	def sendMessage(self,celphone, msg):
 		message = self.client.messages.create(
